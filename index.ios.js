9c74b6d976ff75eed31341c88c6ed405e8f9f3a3
xylxpm <bobo_xylxpm@163.com>
2017-07-26 15:58:22 +0800

    111

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,34 @@
 require('./Root');
+
+// import {AppRegistry, View, Text,} from 'react-native';
+// import React, {Component} from 'react';
+//
+// import ButtonIcon from 'react-native-vector-icons/Ionicons';
+//
+//
+// export default class Root extends Component {
+//     constructor(props) {
+//         super(props);
+//
+//     }
+//
+//     render() {
+//         return (
+//
+//             <View>
+//                <Text>234</Text>
+//                 <ButtonIcon
+//                     name={'ios-mail'}
+//                     size={50}
+//                     style={{ color: '#000000'}}
+//                 />
+//             </View>
+//
+//
+//         );
+//     }
+// };
+//
+// AppRegistry.registerComponent('MyApp', () => Root);
+
+
